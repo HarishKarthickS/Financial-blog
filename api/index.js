731121db1ea58011f81f86051026bbd658b32515
HarishKarthickS <harishskarthick.s@gmail.com>
2024-06-06 19:03:09 +0530

    Bugfix in backend

--- api/index.js
+++ api/index.js
@@ -80,7 +80,6 @@ app.post('/post', uploadMiddleware.single('file'), async (req,res) => {
       title,
       summary,
       content,
-      cover:newPath,
       author:info.id,
     });
     res.json(postDoc);
