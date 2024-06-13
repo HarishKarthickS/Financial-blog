3a2f2a9a7eaffd42bd70ce14284a21fba31c023f
HarishKarthickS <141898754+HarishKarthickS@users.noreply.github.com>
2024-06-13 11:12:03 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -14,7 +14,7 @@ const fs = require('fs');
 const salt = bcrypt.genSaltSync(10);
 const secret = 'asdfe45we45w345wegw345werjktjwertkj';
 
-app.use(cors({credentials:true,origin:['http://localhost:3000','https://financial-blog-alpha.vercel.app/']}));
+app.use(cors({credentials:true,origin:['http://localhost:3000','']}));
 
 
 app.use(express.json());
